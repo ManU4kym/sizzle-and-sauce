62d888d65b761468c34b943a69be7229a8c5ffef
Immanuel Njoroge <emmannjoroge36@gmail.com>
2024-03-10 00:48:10 +0300

    feat: Implement FirebaseUserRepo for user authentication and data management
    This commit introduces the FirebaseUserRepo class, implementing the UserRepoo interface for user authentication and data management. The class leverages Firebase services for authentication and Firestore for storing user data.
    
    New Features:
    - Stream user data using RxDart and Firebase authStateChanges.
    - Implement signIn, signUp, and logOut methods for user authentication.
    - setUserData method for storing user data in Firestore.
    
    This class acts as a bridge between the app and Firebase services, providing seamless integration for user-related operations.

--- packagees/user_repoo/lib/src/firebase_user.dart
+++ packagees/user_repoo/lib/src/firebase_user.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repoo/src/entities/entities.dart';
+import 'package:user_repoo/src/models/user.dart';
 import 'package:user_repoo/src/user_repo.dart';
 
 class FirebaseUserRepo implements UserRepoo {
@@ -11,30 +16,52 @@ class FirebaseUserRepo implements UserRepoo {
   }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
   @override
-  Future<void> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
+  Stream<MyUser?> get user {
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async* {
+      if (firebaseUser == null) {
+        yield MyUser.empty;
+      }
+      yield await usersCollection.doc(firebaseUser!.uid).get().then((value) =>
+          MyUser.fromEntity(MyUserEntity.fromDocument(value.data()!)));
+    });
   }
 
   @override
-  Future<void> setUserData(user) {
-    // TODO: implement setUserData
-    throw UnimplementedError();
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      log(e.toString() as num); // Check this not supposed to be casted
+      rethrow;
+    }
   }
 
   @override
-  Future<void> signIn(String email, String password) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: myUser.email, password: password);
+      myUser.userId = user.user!.uid;
+      return myUser;
+    } catch (e) {
+      log(e.toString() as num); // Check this not supposed to be casted
+      rethrow;
+    }
   }
 
   @override
-  Future<dynamic> signUp(myUser, String password) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<void> logOut() async {
+    await _firebaseAuth.signOut();
   }
 
   @override
-  // TODO: implement user
-  Stream<dynamic> get user => throw UnimplementedError();
+  Future<void> setUserData(MyUser user) async {
+    try {
+      await usersCollection.doc(user.userId).set(user.toEntity().toDocument());
+    } catch (e) {
+      log(e.toString() as num); // Check this not supposed to be casted
+      rethrow;
+    }
+  }
 }
--- packagees/user_repoo/lib/src/models/user.dart
+++ packagees/user_repoo/lib/src/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:user_repoo/src/entities/entities.dart';
+
 class MyUser {
   String userId;
   String email;
