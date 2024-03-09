c59139dcf3c44201a73b59ab2f58f1e383b1317f
Immanuel Njoroge <emmannjoroge36@gmail.com>
2024-03-09 23:27:55 +0300

    feat: Add static factory method 'fromDocument' to MyUserEntity
    This commit introduces a static factory method 'fromDocument' in the MyUserEntity class. This method facilitates the creation of MyUserEntity instances from document data retrieved from a Firestore database.
    
    The method extracts userId, email, name, and hasActiveCart from a Map<String, dynamic> representing a Firestore document, allowing for convenient conversion between Firestore documents and MyUserEntity objects.
    
    Usage example:
    `dart
    MyUserEntity user = MyUserEntity.fromDocument(documentData);

--- packagees/user_repoo/lib/src/entities/entities.dart
+++ packagees/user_repoo/lib/src/entities/entities.dart
@@ -13,12 +13,20 @@ class MyUserEntity {
     required this.hasActiveCart,
   });
 
-  Map<String, Object> toDocument() {
+  Map<String, Object?> toDocument() {
     return {
       'userId': userId,
       'email': email,
       'name': name,
       'hasActiveCart': hasActiveCart,
     };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntity(
+        userId: doc['userId'],
+        email: doc['email'],
+        name: doc['name'],
+        hasActiveCart: doc['hasActiveCart']);
   }
 }
