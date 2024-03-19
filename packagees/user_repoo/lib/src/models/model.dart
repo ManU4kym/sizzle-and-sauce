5559e6ff9911be07cc19f5c3c416a776aa50d89f
Immanuel Njoroge <emmannjoroge36@gmail.com>
2024-03-19 21:59:27 +0300

    pubspec fd fixed

--- packagees/user_repoo/lib/src/models/model.dart
+++
@@ -1,2 +0,0 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-export "user.dart";
--- packagees/user_repoo/lib/src/user_repository.dart
+++
@@ -1,6 +0,0 @@
-library user_repository;
-
-export 'entities/entities.dart';
-export 'models/model.dart';
-export 'user_repo.dart';
-export 'firebase_user.dart';
