ecb1e850db8efc5b7a52dbe1dd73fee8d9805b2d
Immanuel Njoroge <emmannjoroge36@gmail.com>
2024-03-20 23:34:32 +0300

    Register page / auth pck

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pizaa/pages/login_page.dart';
 import 'package:pizaa/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -17,10 +18,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home:  LoginPage(),
+      home: LoginPage(onTap: (){},)
     );
   }
 }
