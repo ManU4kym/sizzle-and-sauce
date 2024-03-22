0169d24941f4443c3c921754e16c12bd1979d95a
Immanuel Njoroge <emmannjoroge36@gmail.com>
2024-03-22 22:27:24 +0300

    reusable drawer tiles

---
+++ lib/components/drawer_tiles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawerTiles extends StatelessWidget {
+  final String text;
+  final IconData? icon;
+  final void Function()? onTap;
+  const DrawerTiles({super.key, required this.text, this.icon, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+      ),
+      onTap: onTap,
+      leading: Icon(
+        icon,
+        color: Theme.of(context).colorScheme.inversePrimary,
+      ),
+    );
+  }
+}
